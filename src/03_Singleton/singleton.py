8ace890fc7913fe323eff1a9a4fd358511e77ef3
snegovik181220 <kirill.zima.zima@gmail.com>
2024-01-16 21:51:31 +1000

    Merge branch 'maruschin:main' into main

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,11 @@
+from typing import Optional
+
+
 class Singleton:
-    pass
+    single: Optional["Singleton"] = None
+
+    def __new__(cls, *args, **kwargs):  # noqa: ARG003
+        if not cls.single:
+            cls.single = super().__new__(cls)
+
+        return cls.single
--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+
 from typing import List
 
 
@@ -15,18 +17,50 @@ class Ocean:
         return f"{self.__class__.__name__}({self.state!r})"
 
     def gen_next_quantum(self) -> "Ocean":
-        raise NotImplementedError
+        num_rows, num_cols = len(self.state), len(self.state[0])
+        new_state = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+        old_state = np.pad(np.array(self.state.copy()), 1)
+        water, fish, shr = 0, 2, 3
+
+        for row in range(1, num_rows+1):
+            for col in range(1, num_cols+1):
+                animals = {i: 0 for i in range(4)}
+
+                for step_x in [-1, 0, 1]:
+                    for step_y in [-1, 0, 1]:
+                        if abs(step_x) + abs(step_y) != 0:
+                            animals[old_state[row+step_x][col+step_y]] += 1
+                        else:
+                            continue
+
+                if (old_state[row][col] == fish) and ((animals[fish] >= 4) or (animals[fish] <= 1)):  # noqa: PLR2004
+                    new_state[row-1][col-1] = water
+
+                elif (old_state[row][col] == shr) and ((animals[shr] >= 4) or (animals[shr] <= 1)):  # noqa: PLR2004
+                    new_state[row-1][col-1] = water
+
+                elif old_state[row][col] == water:
+                    if animals[fish] == 3:  # noqa: PLR2004
+                        new_state[row-1][col-1] = 2
+                    elif animals[shr] == 3:  # noqa: PLR2004
+                        new_state[row-1][col-1] = 3
+                    else:
+                        new_state[row-1][col-1] = old_state[row][col]
+
+                else:
+                    new_state[row-1][col-1] = old_state[row][col]
+
+        return Ocean(new_state)
 
 
 if __name__ == "__main__":
     n_quantums = int(sys.stdin.readline())
     n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
-    init_state = []
+    init_state_ = []
     for i in range(n_rows):
         line = [int(i) for i in sys.stdin.readline().split()]
-        init_state.append(line)
+        init_state_.append(line)
 
-    ocean = Ocean(init_state=init_state)
+    ocean = Ocean(init_state=init_state_)
     for _ in range(n_quantums):
         ocean = ocean.gen_next_quantum()
-    print(ocean)
