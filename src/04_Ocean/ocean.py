23aea97659d6dab1e68641f9f82505ef19c874d5
snegovik181220 <kirill.zima.zima@gmail.com>
2024-01-17 10:43:10 +1000

    Merge branch 'maruschin:main' into main

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -1,4 +1,4 @@
-import sys
+import sys  # noqa: I001
 import numpy as np
 
 from typing import List
@@ -10,81 +10,57 @@ class Ocean:
     def __init__(self, init_state: List[List[int]]):
         self.state = init_state
 
-    import sys
-    from typing import List
+    def __str__(self) -> str:
+        return "\n".join(["".join(str(el) for el in row) for row in self.state])
 
-    class Ocean:
-        state: List[List[int]]
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.state!r})"
 
-        def __init__(self, init_state: List[List[int]]):
-            self.state = init_state
+    def gen_next_quantum(self) -> "Ocean":
+        num_rows, num_cols = len(self.state), len(self.state[0])
+        new_state = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+        old_state = np.pad(np.array(self.state.copy()), 1)
+        water, fish, shr = 0, 2, 3
 
-        def __str__(self) -> str:
-            return "\n".join([" ".join(str(el) for el in row) for row in self.state])
+        for row in range(1, num_rows+1):
+            for col in range(1, num_cols+1):
+                animals = {i: 0 for i in range(4)}
 
-        def __repr__(self) -> str:
-            return f"{self.__class__.__name__}({self.state!r})"
-
-        def gen_next_quantum(self) -> "Ocean":
-            new_state = []
-            fish = 2
-            shrimp = 3
-
-            for i in range(len(self.state)):
-                new_row = []
-                for j in range(len(self.state[i])):
-                    if self.state[i][j] == 1:
-                        new_row.append(1)
-                    else:
-                        n_fish = 0
-                        n_shrimp = 0
-                        neighbors = [
-                            (i - 1, j - 1),
-                            (i - 1, j),
-                            (i - 1, j + 1),
-                            (i, j - 1),
-                            (i, j + 1),
-                            (i + 1, j - 1),
-                            (i + 1, j),
-                            (i + 1, j + 1),
-                        ]
-                        for ni, nj in neighbors:
-                            if ni < 0 or nj < 0 or ni >= len(self.state) or nj >= len(self.state[i]):
-                                continue
-                            if self.state[ni][nj] == fish:
-                                n_fish += 1
-                            elif self.state[ni][nj] == shrimp:
-                                n_shrimp += 1
-
-                        if self.state[i][j] == fish:
-                            if n_fish < 2 or n_fish > 3:  # noqa: PLR2004
-                                new_row.append(0)
-                            else:
-                                new_row.append(2)
-                        elif self.state[i][j] == shrimp:
-                            if n_shrimp < 2 or n_shrimp > 3:  # noqa: PLR2004
-                                new_row.append(0)
-                            else:
-                                new_row.append(3)
-                        elif n_fish == fish and n_shrimp == shrimp:
-                            new_row.append(2)
-                        elif n_fish == 3:  # noqa: PLR2004
-                            new_row.append(2)
+                for step_x in [-1, 0, 1]:
+                    for step_y in [-1, 0, 1]:
+                        if abs(step_x) + abs(step_y) != 0:
+                            animals[old_state[row+step_x][col+step_y]] += 1
                         else:
-                            new_row.append(0)
-                new_state.append(new_row)
+                            continue
 
-            return Ocean(init_state=new_state)
+                if (old_state[row][col] == fish) and ((animals[fish] >= 4) or (animals[fish] <= 1)):  # noqa: PLR2004
+                    new_state[row-1][col-1] = water
 
-    if __name__ == "__main__":
-        n_quantums = int(sys.stdin.readline())
-        n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
-        init_state = []
-        for i in range(n_rows):
-            line = [int(i) for i in sys.stdin.readline().split()]
-            init_state.append(line)
+                elif (old_state[row][col] == shr) and ((animals[shr] >= 4) or (animals[shr] <= 1)):  # noqa: PLR2004
+                    new_state[row-1][col-1] = water
 
-        ocean = Ocean(init_state=init_state)
-        for _ in range(n_quantums):
-            ocean = ocean.gen_next_quantum()
-        print(ocean)  # noqa: T201
+                elif old_state[row][col] == water:
+                    if animals[fish] == 3:  # noqa: PLR2004
+                        new_state[row-1][col-1] = 2
+                    elif animals[shr] == 3:  # noqa: PLR2004
+                        new_state[row-1][col-1] = 3
+                    else:
+                        new_state[row-1][col-1] = old_state[row][col]
+
+                else:
+                    new_state[row-1][col-1] = old_state[row][col]
+
+        return Ocean(new_state)
+
+
+if __name__ == "__main__":
+    n_quantums = int(sys.stdin.readline())
+    n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
+    init_state_ = []
+    for i in range(n_rows):
+        line = [int(i) for i in sys.stdin.readline().split()]
+        init_state_.append(line)
+
+    ocean = Ocean(init_state=init_state_)
+    for _ in range(n_quantums):
+        ocean = ocean.gen_next_quantum()
