f52711e871d6ac0ae90bc34b708a1799914e95d2
snegovik181220 <kirill.zima.zima@gmail.com>
2024-01-17 10:46:08 +1000

    Merge branch 'maruschin:main' into main

--- src/01_Calculator/calculator.py
+++ src/01_Calculator/calculator.py
@@ -1,4 +1,5 @@
 from operator import add, mul, sub, truediv
+from operator import add, mul, sub, truediv
 from typing import List
 
 ops = {"+": add, "-": sub, "*": mul, "/": truediv}
--- src/01_Calculator/test_calculator.py
+++ src/01_Calculator/test_calculator.py
@@ -2,6 +2,24 @@ import pytest
 from calculator import calculate, prefix_evaluate, to_prefix
 
 
+def test_prefix_evaluate():
+    assert prefix_evaluate("+ 2 3".split()) == 5, "Must be 5"
+    assert prefix_evaluate("+ - 2 3 5".split()) == 4, "Must be 4"
+
+
+def test_to_prefix():
+    assert to_prefix("1 + ( 2 - 3 ) * 2") == ["+", "1", "*", "-", "2", "3", "2"]
+
+
+def test_calculate():
+    assert calculate("1 + ( 2 - 3 ) * 2") == eval("1 + ( 2 - 3 ) * 2")  # noqa: S307
+
+
+test_prefix_evaluate()
+test_to_prefix()
+test_calculate()
+
+
 @pytest.mark.parametrize(
     ("equation", "result"),
     [
@@ -26,7 +44,7 @@ def test_prefix_evaluate(equation: str, result: int):
     ],
 )
 def test_to_prefix(infix_equation: str, prefix_equation: str):
-    assert to_prefix(infix_equation) == prefix_equation
+    assert to_prefix(infix_equation) == prefix_equation.split()
 
 
 @pytest.mark.parametrize("equation", ["1 + 2", "1 * 2", "1 + ( 2 - 3 ) * 2"])
